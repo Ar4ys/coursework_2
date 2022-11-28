93367d59fa5fbe64992a6ca60c00e09954f1781f
Ar4ys <ar4ys@pm.me>
2022-11-28 23:39:06 +0100

    Created basic components/Text

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -4,9 +4,9 @@ import { clsx } from 'clsx'
 import styles from './Button.module.css'
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  mode?: 'primary' | 'secondary'
+  variant?: 'primary' | 'secondary'
 }
 
-export const Button: FC<ButtonProps> = ({ mode = 'primary', className, ...props }) => {
-  return <button className={clsx(styles.button, styles[mode], className)} {...props} />
+export const Button: FC<ButtonProps> = ({ variant = 'primary', className, ...props }) => {
+  return <button className={clsx(styles.button, styles[variant], className)} {...props} />
 }
---
+++ app/components/Text/Text.tsx
@@ -0,0 +1,18 @@
+'use client'
+import clsx from 'clsx'
+import React, { FC } from 'react'
+import styles from './Text.module.css'
+
+interface HeadingProps extends React.HTMLAttributes<HTMLHeadingElement> {
+  variant: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+}
+
+interface ParagraphProps extends React.HTMLAttributes<HTMLParagraphElement> {
+  variant?: 'p'
+}
+
+type TextProps = HeadingProps | ParagraphProps
+
+export const Text: FC<TextProps> = ({ variant: Variant = 'p', className, ...props }) => {
+  return <Variant className={clsx(styles[Variant], className)} {...props} />
+}
