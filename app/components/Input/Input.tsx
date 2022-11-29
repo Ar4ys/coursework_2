47edd231d1184cf03813b74bd0af4fa08a9e97ac
Ar4ys <ar4ys@pm.me>
2022-11-29 02:03:59 +0100

    Created basic work reports form

--- app/components/Input/Input.tsx
+++ app/components/Input/Input.tsx
@@ -1,4 +1,5 @@
 'use client'
+import clsx from 'clsx'
 import type { FC } from 'react'
 import { Field, FieldProps } from '../Field'
 import type { TextProps } from '../Text'
@@ -6,14 +7,22 @@ import styles from './Input.module.css'
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string
+  inputClassName?: string
   labelProps?: TextProps
   wrapperProps?: FieldProps
 }
 
-export const Input: FC<InputProps> = ({ label, labelProps, wrapperProps, ...props }) => {
+export const Input: FC<InputProps> = ({
+  label,
+  labelProps,
+  wrapperProps,
+  className,
+  inputClassName,
+  ...props
+}) => {
   return (
-    <Field {...wrapperProps} labelProps={labelProps} label={label}>
-      <input title={label} {...props} />
+    <Field {...wrapperProps} labelProps={labelProps} label={label} className={className}>
+      <input title={label} {...props} className={clsx(styles.input, inputClassName)} />
     </Field>
   )
 }
--- app/components/Select/Select.tsx
+++ app/components/Select/Select.tsx
@@ -1,4 +1,5 @@
 'use client'
+import clsx from 'clsx'
 import type { FC } from 'react'
 import { Field, FieldProps } from '../Field'
 import type { TextProps } from '../Text'
@@ -6,6 +7,7 @@ import styles from './Select.module.css'
 
 interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
   label?: string
+  selectClassName?: string
   labelProps?: TextProps
   wrapperProps?: FieldProps
   options: Array<OptionProps>
@@ -13,10 +15,18 @@ interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
 
 interface OptionProps extends React.OptionHTMLAttributes<HTMLOptionElement> {}
 
-export const Select: FC<SelectProps> = ({ label, labelProps, wrapperProps, options, ...props }) => {
+export const Select: FC<SelectProps> = ({
+  label,
+  labelProps,
+  wrapperProps,
+  options,
+  className,
+  selectClassName,
+  ...props
+}) => {
   return (
-    <Field {...wrapperProps} labelProps={labelProps} label={label}>
-      <select {...props}>
+    <Field {...wrapperProps} labelProps={labelProps} label={label} className={className}>
+      <select {...props} className={clsx(styles.select, selectClassName)}>
         {options.map(optionProps => (
           <option key={optionProps.value?.toString()} {...optionProps}>
             {optionProps.title ?? optionProps.value}
